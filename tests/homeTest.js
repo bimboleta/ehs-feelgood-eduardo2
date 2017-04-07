4c17813894de6dae859256b063506179750aeb63
Eduardo <eduardoruocco@gmail.com>
2017-04-07 09:47:06 -0300

    a

--- tests/homeTest.js
+++ tests/homeTest.js
@@ -25,6 +25,58 @@ module.exports = {
     browser.expect.element("body").text.to.contain("Ver contratos");
     browser.end();
   },
+  "Consegue ver os contratos de um cliente": (browser) => {
+    browser
+      .url("http://localhost:3000/loginGerente")
+      .waitForElementVisible("body", 100);
+    browser.click("#clientes a:nth-of-type(1)").pause(1000);
+    browser.expect.element("body").text.to.contain("Pintar parede");
+    browser.expect.element("body").text.to.contain("Consertar piso");
+    browser.end();
+  },
+  "Consegue ver os contratos de outro integrador": (browser) => {
+    browser
+      .url("http://localhost:3000/loginGerente")
+      .waitForElementVisible("body", 100);
+    browser.click("#integradores tr:nth-of-type(3) a:nth-of-type(1)").pause(1000);
+    browser.expect.element("body").text.to.contain("Pintar parede");
+    browser.end();
+  },
+  "Consegue ver os contratos de um integrador": (browser) => {
+    browser
+      .url("http://localhost:3000/loginGerente")
+      .waitForElementVisible("body", 100);
+    browser.click("#integradores a:nth-of-type(1)").pause(1000);
+    browser.expect.element("body").text.to.contain("Consertar piso");
+    browser.end();
+  },
+  "Consegue ver os contratos de serviço específico de um integrador": (browser) => {
+    browser
+      .url("http://localhost:3000/loginGerente")
+      .waitForElementVisible("body", 100);
+    browser.click("#integradores tr:nth-of-type(3) a:nth-of-type(2)").pause(1000);
+    browser.expect.element("body").text.to.contain("Integrador");
+    browser.expect.element("body").text.to.contain("Fornecedor");
+    browser.end();
+  },
+  "Consegue ver os contratos de serviço específico de um fornecedor": (browser) => {
+    browser
+      .url("http://localhost:3000/loginGerente")
+      .waitForElementVisible("body", 100);
+    browser.click("#fornecedores tr:nth-of-type(3) a:nth-of-type(1)").pause(1000);
+    browser.expect.element("body").text.to.contain("Integrador");
+    browser.expect.element("body").text.to.contain("Fornecedor");
+    browser.end();
+  },
+  "Consegue ver os contratos de serviço específico de outro fornecedor": (browser) => {
+    browser
+      .url("http://localhost:3000/loginGerente")
+      .waitForElementVisible("body", 100);
+    browser.click("#fornecedores tr:nth-of-type(2) a:nth-of-type(1)").pause(1000);
+    browser.expect.element("body").text.to.contain("Integrador");
+    browser.expect.element("body").text.to.contain("Fornecedor");
+    browser.end();
+  },
   "Verifica se há tarifações para o gerente": function (browser) {
     browser
       .pause(15000)
